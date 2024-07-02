4ce9193db583ef03b8d4ee4cd1ea0f44e6dbb1c3
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2024-07-02 17:47:27 -0600

    added functionality to toggle flight data sending.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from kaspersmicrobit import KaspersMicrobit
 import time
 
-from kaspersmicrobit import KaspersMicrobit
-
+recieving_data = False
 
 def pressed(button):
-    print(f"button {button} pressed")
+
+    if button == "A":
+        recieving_data = True
+        print(recieving_data)
+
+    if button == "B":
+        recieving_data = False
+        print(recieving_data)
 
 with KaspersMicrobit.find_one_microbit() as microbit:
+    # listen for button events / luister naar drukken op knoppen
     microbit.buttons.on_button_a(press=pressed)
-    time.sleep(10)
+    microbit.buttons.on_button_b(press=pressed)
+    time.sleep(15)
